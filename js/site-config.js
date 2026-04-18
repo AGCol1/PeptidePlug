e975c2ae7a4457baa513299a01f8aed3950dc1a8
AGCol1 <agcolclough1@gmail.com>
2026-04-18 18:27:01 +0100

    announcement banner change

--- js/site-config.js
+++ js/site-config.js
@@ -1,4 +1,4 @@
 window.SITE_CONFIG = {
-  announcementText: "Free UK Shipping on Orders Over £100 | Same Day Dispatch | 24/7 Customer Support",
+  announcementText: "Free UK Shipping on Orders Over £100 | Order before 4PM - Same Day Dispatch | 24/7 Customer Support",
   shopifyDomain: "peptide-plug.myshopify.com"
 };
